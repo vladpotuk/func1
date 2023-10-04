7d4cd44d447e81f31dac37002f36801941be1f0c
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2023-10-04 07:39:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def display_show(author):
+    show = "Don't compare yourself with anyone in this world…\nif you do so, you are insulting yourself."
+    formatted_text = f'"{show}"\n{author}'
+    print(formatted_text)
+
+author = "Bill Gates"
+display_show(author)
